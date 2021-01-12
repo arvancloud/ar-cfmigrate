ee34fe4fcc3dcd652a0a7bd5346961444cc9d9f4
Mohammad Hossein Karami <m.karami@asax.local>
2021-01-12 16:41:58 +0330

    add base api to services

--- CfMigrate/Arvancloud/ArvancloudService.cs
+++ CfMigrate/Arvancloud/ArvancloudService.cs
@@ -3,10 +3,13 @@
     public class ArvancloudService : IArvancloudService
     {
         private readonly string _token;
-        
+        private readonly string _baseApi;
+        private const string ApiVersion = "4.0";
+
         public ArvancloudService(string token)
         {
             _token = token;
+            _baseApi = "https://napi.arvancloud.com/cdn/" + ApiVersion + "/";
         }
     }
 }
--- CfMigrate/Cloudflare/CloudflareService.cs
+++ CfMigrate/Cloudflare/CloudflareService.cs
@@ -3,10 +3,14 @@
     public class CloudflareService : ICloudflareService
     {
         private readonly string _token;
-        
+        private readonly string _baseApi;
+        private const string ApiVersion = "v4";
+
         public CloudflareService(string token)
         {
             _token = token;
+
+            _baseApi = "https://api.cloudflare.com/client/" + ApiVersion + "/";
         }
     }
 }
