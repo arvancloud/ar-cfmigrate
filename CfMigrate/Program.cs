ba4d36b7afd443fb548843436bffa1aef6dc6244
Mohammad Hossein Karami <m.karami@asax.local>
2021-01-12 16:24:32 +0330

    init project

---
+++ CfMigrate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CfMigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
