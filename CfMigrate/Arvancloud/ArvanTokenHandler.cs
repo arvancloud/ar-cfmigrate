4e60a088b5c977dd6402300d8f07bd8b00aef7b8
Mohammad Hossein Karami <m.karami@asax.local>
2021-01-13 09:13:58 +0330

    change token handler method

--- CfMigrate/Arvancloud/ArvanTokenHandler.cs
+++ CfMigrate/Arvancloud/ArvanTokenHandler.cs
@@ -1,32 +1,17 @@
 ﻿namespace CfMigrate.Arvancloud
 {
-    public class ArvanTokenHandler
+    public static class ArvanTokenHandler
     {
         private static string _token;
-        private static readonly object Lock = new();
-        private static ArvanTokenHandler _tokenHandler;
-
-        private ArvanTokenHandler(string token)
-        {
-            _token = token;
-        }
-
-        public static ArvanTokenHandler Instance(string token)
-        {
-            if (_tokenHandler is null)
-            {
-                lock (Lock)
-                {
-                    _tokenHandler ??= new ArvanTokenHandler(token);
-                }
-            }
-
-            return _tokenHandler;
-        }
 
         public static string GetToken()
         {
             return _token;
+        }
+
+        public static void SetToken(string token)
+        {
+            _token = token;
         }
     }
 }
--- CfMigrate/Cloudflare/CloudflareTokenHandler.cs
+++ CfMigrate/Cloudflare/CloudflareTokenHandler.cs
@@ -1,32 +1,17 @@
 ﻿namespace CfMigrate.Cloudflare
 {
-    public class CloudflareTokenHandler
+    public static class CloudflareTokenHandler
     {
         private static string _token;
-        private static readonly object Lock = new();
-        private static CloudflareTokenHandler _tokenHandler;
-
-        private CloudflareTokenHandler(string token)
-        {
-            _token = token;
-        }
-
-        public static CloudflareTokenHandler Instance(string token)
-        {
-            if (_tokenHandler is null)
-            {
-                lock (Lock)
-                {
-                    _tokenHandler ??= new CloudflareTokenHandler(token);
-                }
-            }
-
-            return _tokenHandler;
-        }
 
         public static string GetToken()
         {
             return _token;
+        }
+
+        public static void SetToken(string token)
+        {
+            _token = token;
         }
     }
 }
